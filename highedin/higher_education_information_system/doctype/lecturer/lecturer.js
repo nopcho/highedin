f91b8beae69b7434e46231f5090ce583b245b222
Nopporn Chotikakamthorn <nop_itkmitl@hotmail.com>
2021-02-26 13:45:40 +0700

    generated highedin modules

--- highedin/higher_education_information_system/doctype/lecturer/lecturer.js
+++ highedin/higher_education_information_system/doctype/lecturer/lecturer.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Nopporn Chotikakamthorn and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Initial', {
+frappe.ui.form.on('Lecturer', {
 	// refresh: function(frm) {
 
 	// }
