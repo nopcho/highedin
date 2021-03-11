5405e5e4f91fc1f39411c66c008fb8e7b7c6816e
taritinth <tarit.khanbo@hotmail.com>
2021-03-11 18:27:45 +0700

    add main docType

---
+++ highedin/facility_management/doctype/facility/facility.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Nopporn Chotikakamthorn and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Facility', {
+	// refresh: function(frm) {
+
+	// }
+});
