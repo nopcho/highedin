07bb7c7e3bf47c250dfd695f8af7f2a2cebc7e32
Kanlaya <film_kanlaya@hotmail.com>
2021-03-15 17:02:54 +0700

    Intitial Commit

--- highedin/program_management/doctype/curriculum_major/curriculum_major.py
+++ highedin/program_management/doctype/curriculum_major/curriculum_major.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class PublicationSource(Document):
+class CurriculumMajor(Document):
 	pass
--- highedin/program_management/doctype/curriculum_plan_option/curriculum_plan_option.py
+++ highedin/program_management/doctype/curriculum_plan_option/curriculum_plan_option.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class PublicationSource(Document):
+class CurriculumPlanOption(Document):
 	pass
