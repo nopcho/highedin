9a2fdfdcf47e2e2de6e1d112cd0bf47b7e0570d6
Nopporn Chotikakamthorn <nop_itkmitl@hotmail.com>
2021-03-28 22:33:31 +0700

    add student profile

--- highedin/program_management/doctype/student_application/student_application.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Nopporn Chotikakamthorn and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Student Application', {
-	// refresh: function(frm) {
-
-	// }
-});
