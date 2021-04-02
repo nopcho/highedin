e2940ccb79a2407f88c0fe63c9ec19fef038ef64
piyanatnine <56292153+piyanatnine@users.noreply.github.com>
2021-04-02 16:51:51 +0700

    Updated detail on child table fields.
    See sws263-qaadm google sheet for updated detail on child table fields.

---
+++ highedin/quality_assurance/doctype/reference_document/reference_document.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Nopporn Chotikakamthorn and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Reference Document', {
+	// refresh: function(frm) {
+
+	// }
+});
