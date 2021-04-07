fba9ed72739242c9334d0420163d77fcfe2de4bd
piyanatnine <56292153+piyanatnine@users.noreply.github.com>
2021-04-08 04:31:25 +0700

    template webview
    create template WedView folder

--- highedin/quality_assurance/doctype/aunqa_program_level_template/aunqa_program_level_template.py
+++ highedin/quality_assurance/doctype/aunqa_program_level_template/aunqa_program_level_template.py
@@ -5,10 +5,10 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class AUNQAProgramLevelTemplate(Document):
+class AUNQAProgramLevelTemplate(WebsiteGenerator):
 	def validate(self):
 		self.aunqa_code = self.name
+		self.route = 'aunqa-program-level-template/'+self.aunqa_code
 		
-
-
