dbfbb33ab9ef1c93fcefeb2a22dc40ee576b27a8
earndiva <earnoad@gmail.com>
2021-04-08 19:29:28 +0700

    Create Doctype, Update detail in child table

---
+++ highedin/quality_assurance/doctype/aunqa_table_x_x/aunqa_table_x_x.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Nopporn Chotikakamthorn and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AUNQA Table x_x', {
+	// refresh: function(frm) {
+
+	// }
+});
