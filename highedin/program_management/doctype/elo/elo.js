5224a7613d2949a4b8ec9af87a9babc741c8f712
Kanlaya <film_kanlaya@hotmail.com>
2021-05-06 16:25:43 +0700

    filter update

--- highedin/program_management/doctype/elo/elo.js
+++ highedin/program_management/doctype/elo/elo.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('Elo', 'onload', function(frm){
 	frm.set_query('elo_type', function() {
 		return {
 		   "filters" : {
-			"type_name" : frm.doc.elo_category
+			"type_name" : frm.doc.elo_catagory
 			}
 		};
 	});
